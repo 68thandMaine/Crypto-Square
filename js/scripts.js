bba57950cdfaf6fd7d76593037156b968802cd58
Chris Rudnicky and Robert Bruce <chris@>
2018-10-25 16:41:09 -0700

    finalize back-end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,59 @@ function wordArr(word) {
 function sentenceArr(sentence) {
   var array = sentence.split(" ");
   var result = [];
-  array.forEach(function(word){
-    debugger;
+  array.forEach(function(word) {
     var tempArr = wordArr(word)
     result = result.concat(tempArr);
   })
   console.log(result)
   return result;
 }
+
+function count(array) {
+  return array.length;
+}
+
+function determineColumns(array) {
+  var root = Math.sqrt(count(array));
+  if (Number.isInteger(root)) {
+    console.log("root = " + root);
+    return root
+    //the width of the square is equal to root
+  } else {
+    root = Math.ceil(root)
+    console.log("root = " + root);
+    return root;
+    //if the root is not an integer, round up to the nearest integer and return
+  }
+}
+
+function encrypt(sentence) {
+  var array = sentenceArr(sentence);
+  var columns = determineColumns(array);
+  var tempArray = [];
+  var tempArray2 = [];
+
+for (var j = 0; j < columns; j++){
+  for (var i = 0; i < array.length; i += columns) {
+    tempArray.push(array[i + j]);
+  };
+  tempArray2.push(tempArray);
+}
+  console.log(tempArray2)
+  return tempArray2[0];
+};
+
+function cleanUp(sentence){
+  var dirtyEncryptedArray = encrypt(sentence);
+  var cleanedArray =[];
+  dirtyEncryptedArray.forEach(function(letter){
+    if (letter){
+      cleanedArray.push(letter);
+    }
+  });
+  for (var i = 0; i < cleanedArray.length; i += 6){
+    cleanedArray.splice(i, 0, " ");
+  }
+  console.log(cleanedArray.join(""));
+  return cleanedArray.join("");
+};
